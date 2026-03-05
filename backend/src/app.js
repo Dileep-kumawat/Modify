020f39ba075377d5e566c4084c0b38ed9e18e576
Dileep <dileepkumawat525@gmail.com>
2026-03-05 21:43:02 +0530

    production fix 6

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,11 +10,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.sendFile("../index.html")
-})
+app.use(express.static("./public"));
 
 /**
  * @route /api/song
